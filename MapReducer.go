0817c7917f66dbe270e753ddb4c6ad3b774df14e
youssefEdrees <44654408+youssefEdrees@users.noreply.github.com>
2021-04-24 19:46:37 +0200

    added safe counter

--- MapReducer.go
+++ MapReducer.go
@@ -10,12 +10,19 @@ import (
 	"sync"
 )
 
+const N = 5
+
+type SafeCounter struct {
+	mu      sync.Mutex
+	counter int
+}
+
 type SafeFreqMap struct {
 	mu    sync.Mutex
 	myMap map[string]int
 }
 
-func frequency(words []string, ch chan map[string]int) {
+func frequency(words []string, ch chan map[string]int, mySafeCounter *SafeCounter, N int) {
 	/*
 		takes strip of strings (words), then sends the frequency of each str into a channel (ch)
 
@@ -30,6 +37,12 @@ func frequency(words []string, ch chan map[string]int) {
 		m[word] += 1
 	}
 	ch <- m
+	mySafeCounter.mu.Lock()
+	mySafeCounter.counter += 1
+	if mySafeCounter.counter >= N {
+		close(ch)
+	}
+	mySafeCounter.mu.Unlock()
 	return
 }
 
@@ -45,7 +58,7 @@ func channelProcessing(ch chan map[string]int) {
 	mainMapStruct := SafeFreqMap{myMap: make(map[string]int)}
 	//x := [5]map[string]int{<-ch, <-ch, <-ch, <-ch, <-ch}
 
-	for i := 0; i < 5; i++ {
+	for i := 0; i < N; i++ {
 		x := <-ch
 		go reducer(&mainMapStruct, x)
 	}
@@ -134,7 +147,7 @@ func rankByWordCount(wordFrequencies map[string]int) PairList {
 }
 
 func main() {
-	ch := make(chan map[string]int, 5)
+	ch := make(chan map[string]int, N)
 
 	dat, err := ioutil.ReadFile("input.txt")
 	if err != nil {
@@ -146,15 +159,17 @@ func main() {
 	text = strings.ReplaceAll(text, "\n", " ")
 	words := strings.Split(text, " ")
 
+	mySafeCounter := SafeCounter{counter: 0}
+
 	size := float32(len(words))
-	for i := 0; i < 5; i++ {
+	for i := 0; i < N; i++ {
 		// splitting
 		start := size * float32(i) / 5.0
 		end := size * float32(i+1) / 5.0
 		wordsSlice := words[int(start):int(end)]
 
 		// calling go routine for current split
-		go frequency(wordsSlice, ch)
+		go frequency(wordsSlice, ch, &mySafeCounter, N)
 	}
 
 	channelProcessing(ch)
